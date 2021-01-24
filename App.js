0af59d5d939e76cf9397c072be54dc131c44e316
Minte-grace <46419626+Minte-grace@users.noreply.github.com>
2021-01-24 09:01:02 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import HomeStackNavigator from "./src/navigations/Navigator"
 import { useFonts, Montserrat_700Bold,Montserrat_600SemiBold,Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat';
 import {AppLoading} from 'expo';
+import {Views} from 'react-native'
 
 const App = () => {
   let [fontsLoaded] = useFonts({
